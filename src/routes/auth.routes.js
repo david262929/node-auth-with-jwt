65e05fdce5d90b9683d1967aec33f4a11b798599
David <davidghazaryan2000@gmail.com>
2020-09-03 22:59:15 +0400

    fixed package name

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,17 @@
+// require('dotenv').config()
+const {Router} = require('express')
+const router = Router()
+
+        // [
+        // check('email', 'Not correct email.').isEmail(),
+        //     check('password', 'Min length of password 6 symbols.').isLength({min: 6}),
+        // ],
+router.post(
+    '/register',
+
+    async (req, res) => {
+
+    }
+);
+
+module.exports = router;
